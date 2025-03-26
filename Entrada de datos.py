3bac2992de51065c05fb2d26c7c865db5ff4a3b7
josetellez09 <josetelpaez09@gmail.com>
2025-03-26 00:34:06 -0500

    version1

---
+++ Entrada de datos.py
@@ -0,0 +1,7 @@
+#Entrada de datos 
+numero_proyectos_Retrasados = int(input("Ingrese el numero de proyectos que ha emtregado con mas de un dia de retraso: "))
+#Formula para calcular el retraso en la entrega del proyecto
+dias_asignados = int(input("Ingrese el numero total de dias que se le asignaron para la entrega del proyecto: "))
+dias_retraso = int(input("Ingrese el numero total de dias de retraso en la entrega del proyecto: "))
+porcentaje_retraso = float
+print(f"El porcentaje total de retraso con respecto a la entrega incial es: {dias_retraso/dias_asignados * 100}")
